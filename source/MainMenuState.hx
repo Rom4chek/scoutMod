f02020ced4e8f0021f3380e225ae94ff6866ff95
Rom4chek <Animation370@gmail.com>
2023-03-27 18:21:07 +0600

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -291,7 +291,7 @@ class MainMenuState extends MusicBeatState
 					add = menuItems.length * 8;
 				}
 				camFollow.setPosition(spr.getGraphicMidpoint().x, spr.getGraphicMidpoint().y - add);
-				//spr.centerOffsets();
+				spr.centerOffsets();
 			}
 		});
 	}
